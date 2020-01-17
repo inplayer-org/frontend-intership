864e2594597fa3df2adcc9565c79b3cdb4ef6218
mustran <ademimusli000@gmail.com>
2020-01-17 14:38:04 +0100

    refactor; extract a function to get the day of week as a String instead of a Number

--- MusliAdemi/Project2/weather-forecast-app/src/components/pages/CityWeather.jsx
+++ MusliAdemi/Project2/weather-forecast-app/src/components/pages/CityWeather.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { days } from '../../data/daysOfWeek'
+
+const getDayOfWeekAsString = (oneDayWeather) => {
+    const unix_timestamp = oneDayWeather.dt;
+    const date = new Date(unix_timestamp * 1000);
+    const dayOfWeek = days[date.getDay()];
+
+    return dayOfWeek;
+}
+
 
 const CityWeather = (props) => {
-    console.log(props);
+
     const { cityName } = props.match.params;
-    console.log(props.history.location.state.cityData);
+    const { cityData } = props.history.location.state;
+
+    const [showFarhenheit, setShowFarhenheit] = useState(false);
+
+
+
     return (
-        <div>
-            City Weather component for {cityName}
+        <div style={{ textAlign: "center" }}>
+            <h1>City Weather component for {cityName}</h1>
+            <button onClick={setShowFarhenheit(!showFarhenheit)}>F</button>
+            {/* iterate through 1 week list of data */}
+            {cityData.list.map(oneDayWeather => {
+                const dayInStringFormat = getDayOfWeekAsString(oneDayWeather);
+                const icon = oneDayWeather.weather[0].icon;
+                return (
+                    <div key={oneDayWeather.dt}>
+                        <img src={`http://openweathermap.org/img/wn/${icon}.png`} />
+                        <p>{`${dayInStringFormat} ${Math.ceil(oneDayWeather.temp.day)} `}</p>
+                    </div>
+                );
+            })}
+            <Link to={'/'}>Go back</Link>
         </div>
     );
 }
