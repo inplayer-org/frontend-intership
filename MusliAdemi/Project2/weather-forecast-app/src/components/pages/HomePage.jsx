144e3ff899aa41172ba60a43eb785047ab93a16a
mustran <ademimusli000@gmail.com>
2020-01-17 14:18:00 +0100

    refactor component; remove unnecessary useEffect hook

--- MusliAdemi/Project2/weather-forecast-app/src/components/pages/HomePage.jsx
+++ MusliAdemi/Project2/weather-forecast-app/src/components/pages/HomePage.jsx
@@ -1,55 +1,46 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
-import CityWeather from './CityWeather';
-
 
 const HomePage = (props) => {
 
     const [cityName, setCityName] = useState("");
     const [celsiusData, setCelsiusData] = useState(null);
-    const [makeAPIcall, setMakeAPIcall] = useState(false);
 
-    useEffect(() => {
-        let celsiusAPILink = `http://api.openweathermap.org/data/2.5//forecast/daily?units=metric&APPID=b714ec74bbab5650795063cb0fdf5fbe&cnt=7&q=${cityName}`;
+    const makeAPIcall = (e) => {
+        e.preventDefault();
+
+        let weatherAPIlinkC = `http://api.openweathermap.org/data/2.5//forecast/daily?units=metric&APPID=b714ec74bbab5650795063cb0fdf5fbe&cnt=7&q=${cityName}`;
 
         const fetchData = async () => {
-            if (cityName) {
-                let response;
-                try {
-                    response = await axios(celsiusAPILink);
-                    setCelsiusData(response.data);
-                    console.log(response.data);
-                    props.history.push({
-                        pathname: `/forecast/${cityName}`,
-                        search: `${cityName}`,
-                        state: { cityData: response.data }
-                    })
-                } catch (e) {
-                    console.log("ERROR");
-                    props.history.push(`${cityName}`)
-                }
+            let response;
+            try {
+                response = await axios(weatherAPIlinkC);
+                setCelsiusData(response.data);
+                props.history.push({
+                    pathname: `/forecast/${cityName}`,
+                    state: { cityData: response.data }
+                })
+            } catch (e) {
+                props.history.push({
+                    pathname: `error/${cityName}`,
+                    search: `${cityName}`
+                })
             }
         };
         fetchData();
-    }, [makeAPIcall]);
-    // console.log(props);
-
-    const handleSubmitAPIcall = e => {
-        e.preventDefault();
-        setMakeAPIcall(!makeAPIcall)
     }
 
     return (
         <div>
             HomePage component - search city
-            <form onSubmit={handleSubmitAPIcall}>
+            <form onSubmit={makeAPIcall}>
                 <input
                     onChange={(e) => setCityName(e.target.value)}
                     type="search"
                     placeholder="city"
                     value={cityName}
                 />
-                <button onClick={handleSubmitAPIcall}>Search City</button>
+                <button onClick={makeAPIcall}>Search City</button>
             </form>
         </div>
     );
