bd864aaf1dc08eae5632f53d393bab2a108cabe9
mustran <ademimusli000@gmail.com>
2020-01-17 15:06:53 +0100

    toggle Celsius and Fahrenheit

--- MusliAdemi/Project2/weather-forecast-app/src/components/pages/CityWeather.jsx
+++ MusliAdemi/Project2/weather-forecast-app/src/components/pages/CityWeather.jsx
@@ -10,28 +10,48 @@ const getDayOfWeekAsString = (oneDayWeather) => {
     return dayOfWeek;
 }
 
-
 const CityWeather = (props) => {
 
     const { cityName } = props.match.params;
     const { cityData } = props.history.location.state;
 
+    const capitalizedCityName = cityName.charAt(0).toUpperCase() + cityName.substring(1).toLowerCase();
+
     const [showFarhenheit, setShowFarhenheit] = useState(false);
+    const [showCelsius, setShowCelsius] = useState(true);
+    const [toggleFC, setToggleFC] = useState(false);
 
+    const handleToggleF = () => {
+        setShowFarhenheit(true);
+        setShowCelsius(false);
+    }
 
+    const handleToggleC = () => {
+        setShowCelsius(true);
+        setShowFarhenheit(false)
+    }
+
+    const celsiusToFarhenheit = (celsiusFormat) => {
+        // Multiply by 9, then divide by 5, then add 32
+        return (celsiusFormat * 9) / 5 + 32;
+    }
 
     return (
         <div style={{ textAlign: "center" }}>
-            <h1>City Weather component for {cityName}</h1>
-            <button onClick={setShowFarhenheit(!showFarhenheit)}>F</button>
+            <h1>City Weather for {capitalizedCityName}</h1>
+            <button onClick={handleToggleF} disabled={showFarhenheit}>F</button>
+            <button onClick={handleToggleC} disabled={showCelsius}>C</button>
             {/* iterate through 1 week list of data */}
             {cityData.list.map(oneDayWeather => {
                 const dayInStringFormat = getDayOfWeekAsString(oneDayWeather);
                 const icon = oneDayWeather.weather[0].icon;
+                const dayTempInCelsius = Math.round(oneDayWeather.temp.day);
+                const dayTempInFarhenheit = Math.round(celsiusToFarhenheit(dayTempInCelsius))
                 return (
                     <div key={oneDayWeather.dt}>
                         <img src={`http://openweathermap.org/img/wn/${icon}.png`} />
-                        <p>{`${dayInStringFormat} ${Math.ceil(oneDayWeather.temp.day)} `}</p>
+                        <p>{`${dayInStringFormat}`}</p>
+                        <p>{showCelsius ? `${dayTempInCelsius} °C` : `${dayTempInFarhenheit} °F`} </p>
                     </div>
                 );
             })}
