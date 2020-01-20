e05db57a577f9c8da241eb119a95ca28641dd094
sandrananceva <sandra.nanceva@gmail.com>
2020-01-20 10:16:00 +0100

    add today  weather icon

--- SandraNancheva/Project2.1/src/components/TodayWeather/todayweather.jsx
+++ SandraNancheva/Project2.1/src/components/TodayWeather/todayweather.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const TodayWeather = ({ data, units }) => (
-	<div>
-		{data.list && <h1>{data.list[0].temp.day} {units==='metric'? 'C': 'F'}</h1>}
-		<h1>icon</h1>
-		{data.list && <p>Day: {data.list[0].temp.day}  {units==='metric'? 'C': 'F'}</p>}
-		{data.list && <p>Night: {data.list[0].temp.night}  {units==='metric'? 'C': 'F'}</p>}
-		{data.list && <p>Evening: {data.list[0].temp.eve}  {units==='metric'? 'C': 'F'}</p>}
-		{data.list && <p>Morning: {data.list[0].temp.morn}  {units==='metric'? 'C': 'F'}</p>}
-	</div>
-);
+const TodayWeather = ({ data, units }) => {
+	
+	return (
+		<div>
+			{data.list && <h1>{data.list[0].temp.day} {units==='metric'? 'C': 'F'}</h1>}
+			{data.list &&<img src={`http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png`} alt="icon"/>}
+			{data.list && <p>Day: {data.list[0].temp.day}  {units==='metric'? 'C': 'F'}</p>}
+			{data.list && <p>Night: {data.list[0].temp.night}  {units==='metric'? 'C': 'F'}</p>}
+			{data.list && <p>Evening: {data.list[0].temp.eve}  {units==='metric'? 'C': 'F'}</p>}
+			{data.list && <p>Morning: {data.list[0].temp.morn}  {units==='metric'? 'C': 'F'}</p>}
+		</div>
+	);
+}
 
 export default TodayWeather
