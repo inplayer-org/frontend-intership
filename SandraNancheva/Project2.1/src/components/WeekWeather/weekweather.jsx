d0c2807f097e464837c8af1337ea17c043f4bea3
sandrananceva <sandra.nanceva@gmail.com>
2020-01-20 10:11:09 +0100

    add weather icons

--- SandraNancheva/Project2.1/src/components/WeekWeather/weekweather.jsx
+++ SandraNancheva/Project2.1/src/components/WeekWeather/weekweather.jsx
@@ -6,11 +6,14 @@ const WeekWeather = ({ day, units}) => {
     console.log("OPTIONS: " + options);
     console.log();
     console.log("DAY " + day);
+    const icon = `http://openweathermap.org/img/wn/${day.weather[0].icon}.png`
+
+    console.log(day.weather[0].icon)
     const dayAsString = date.toLocaleDateString(undefined,options);
 	return (
 		<div>
 		    <h1>{dayAsString}</h1>
-            <h3>icon</h3>
+            <img src={icon} alt="icon"/>
             <h1>{day.temp.day} {units==='metric'? 'C': 'F'}</h1>
         </div>
        
