54142ce2ceafddce090151f442508e90d747695d
lalkovski <ilija.lalkovski@inplayer.com>
2020-02-05 13:48:25 +0100

    Add redux-persist to project, when refreshed, state will not reset

--- IlijaLalkovski/Project2/src/redux/root-reducer.js
+++ IlijaLalkovski/Project2/src/redux/root-reducer.js
@@ -1,7 +1,17 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import forecastReducer from './forecast/forecast.reducer';
 
-export default combineReducers({
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: [ 'forecast' ]
+};
+
+const rootReducer = combineReducers({
 	forecast: forecastReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
