e3fd8032f8acb05377c9e6a4aad175e7e7155ee7
lalkovski <ilija.lalkovski2301@gmail.com>
2020-02-08 00:01:54 +0100

    Clean up code, remove clutter

--- IlijaLalkovski/Project2/src/App.js
+++ IlijaLalkovski/Project2/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
@@ -6,43 +6,14 @@ import HomePage from './pages/home-page/home-page.component';
 import ForecastPage from './pages/forecast-page/forecast-page.component';
 import ErrorPage from './pages/error-page/error-page.component';
 
-class App extends Component {
-	clearSuccess = () => {
-		this.setState({ isSuccess: false });
-	};
-
-	clearState = () => {
-		this.setState({ isFail: false, city: '' });
-	};
-
-	render() {
-		return (
-			<div className="App">
-				<Switch>
-					<Route exact path="/" component={HomePage} />
-					<Route
-						path="/forecast"
-						render={(props) => (
-							<ForecastPage
-								{...props}
-								{...this.state}
-								clearSuccess={this.clearSuccess}
-							/>
-						)}
-					/>
-					<Route
-						path="/404notfound"
-						render={(props) => (
-							<ErrorPage
-								{...props}
-								clearState={this.clearState}
-							/>
-						)}
-					/>
-				</Switch>
-			</div>
-		);
-	}
-}
+const App = () => (
+	<div className="App">
+		<Switch>
+			<Route exact path="/" component={HomePage} />
+			<Route path="/forecast" component={ForecastPage} />
+			<Route path="/404notfound" component={ErrorPage} />
+		</Switch>
+	</div>
+);
 
 export default App;
